b1a100b1f12f188594112457f258986129ee93de
fangyonghao <fang_yonghao@163.com>
2019-01-27 19:30:37 +0800

    add bubblesort

---
+++ sort/bubblesort.cc
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void BubbleSort(int num[], int length) {
+    if(NULL == num || length <= 1) {
+        return;
+    }
+    for(int j = length; j > 1; j--) {
+        for(int i = 0; i < j - 1; i++) {
+            if(num[i]>num[i+1]) {
+                int temp = num[i+1];
+                num[i+1] = num[i];
+                num[i] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int a[] = {4, 7, 9, 13, 1, 8, 2};
+    BubbleSort(a, 6);
+    for(int i = 0; i < 6; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    return 0;
+    
+}
