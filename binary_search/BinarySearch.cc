8164855085849bd5e7096ec5dff6324a370285da
fangyonghao <fang_yonghao@163.com>
2019-02-05 10:36:12 +0800

    add BinarySearch

---
+++ binary_search/BinarySearch.cc
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int BinarySearch(int array[], int arraylen, int target) {
+    int start = 0;
+    int end = arraylen - 1;
+    int mid = (start + end)/2;
+    while(start <= end) {
+        if(array[mid] > target) {
+            end = mid - 1;
+        } else if (array[mid] < target) {
+            start = mid + 1;
+        } else {
+            return mid;
+        }
+        mid = (start + end)/2;
+    }
+    return -1;
+}
+
+int main() {
+    int a[] = {3, 5, 10, 13, 15, 20, 21};
+    printf("%d\n", BinarySearch(a, 7, 23));
+    return 0;
+}
