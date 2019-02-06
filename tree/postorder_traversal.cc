dacb1f2eac60717210bda0206f0c44c67aff1ed4
fangyonghao <fang_yonghao@163.com>
2019-02-06 10:30:41 +0800

    post order

---
+++ tree/postorder_traversal.cc
@@ -0,0 +1,37 @@
+#include <stack>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x): val(x), left(NULL), right(NULL) {}
+};
+
+vector<int> PostorderTraversal(TreeNode* root) {
+    vector<int> result;
+    if(NULL == root) {
+        return result;
+    }
+    stack<TreeNode*> s;
+    TreeNode* curr = root;
+    TreeNode* previsited = NULL;
+    while(curr!=NULL || !s.empty()) {
+        while(curr!=NULL) {
+            s.push(curr);
+            curr = curr->left;
+        }
+        curr = s.top();
+        if(curr->right == NULL || curr==previsited) {
+            result.push_back(curr->val);
+            s.pop();
+            previsited = curr;
+            curr = NULL;
+        } else {
+            curr = curr->right;
+        }
+    }
+
+    return result;
+}
